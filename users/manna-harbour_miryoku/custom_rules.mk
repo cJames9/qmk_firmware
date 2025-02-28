3b15f55eb0f658ad7d37e49a056ebff948eea47a
Cleison de Oliveira Padilha <cleison_lp@hotmail.com>
2025-02-28 17:25:43 -0300

    misc review

--- users/manna-harbour_miryoku/custom_rules.mk
+++ users/manna-harbour_miryoku/custom_rules.mk
@@ -6,7 +6,7 @@ WPM_ENABLE           = yes   # WPM Calculation
 # CAPS_WORD_ENABLE 	 = yes   # LShift + RShift enables caps for a single word
 CONSOLE_ENABLE 		 = no
 COMMAND_ENABLE       = no
-CONVERT_TO			 = helios
+DEBOUNCE_TYPE        = sym_eager_pk
 
 ifeq ($(strip $(OLED_ENABLE)), yes)
     SRC += oled.c
