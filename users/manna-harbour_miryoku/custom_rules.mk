020a4bf192100657593b7de06251022e3db3375b
Cleison de Oliveira Padilha <cleison_lp@hotmail.com>
2025-09-23 21:46:24 -0300

    reviewed lumberjack activation

--- users/manna-harbour_miryoku/custom_rules.mk
+++ users/manna-harbour_miryoku/custom_rules.mk
@@ -6,6 +6,7 @@ WPM_ENABLE           = yes   # WPM Calculation
 # CAPS_WORD_ENABLE 	 = yes   # LShift + RShift enables caps for a single word
 CONSOLE_ENABLE 		 = no
 COMMAND_ENABLE       = no
+LUMBERJACK_ENABLE = yes
 
 ifeq ($(strip $(OLED_ENABLE)), yes)
     SRC += oled.c
